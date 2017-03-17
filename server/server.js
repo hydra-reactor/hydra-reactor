ceb54e3fdf005acfa893a812232fa9cc317d66a2
Ryan Freude <freudehack@gmail.com>
2017-03-17 11:06:07 -0700

    temp commit

--- server/server.js
+++ server/server.js
@@ -3,18 +3,14 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
 var Activity = require('./activity-model.js');
-
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 //mongoose.connect('mongodb://localhost/hydra');
 var db = mongoose.connection;
-
 app.use(express.static(path.join(__dirname, '../client/')));
-
-// Set up GET request listener for retrieving links
+//Set up GET request listener for retrieving links
 app.get('/api/activities', function(req, res) {
   console.log('Server GET req.body: ', req.body);
   // Call Mongoose find method to retrieve all activities
@@ -26,7 +22,6 @@ app.get('/api/activities', function(req, res) {
     }
   });
 });
-
 // Set up POST request listener for adding an activity
 app.post('/api/activities', function(req, res) {
   console.log('Server POST req.body: ', req.body);
@@ -39,7 +34,6 @@ app.post('/api/activities', function(req, res) {
     }
   });
 });
-
 // Set up DELETE request listener from deleting an activity
 app.delete('/api/activities', function(req, res) {
   console.log('Server DELETE req.body: ', req.body);
@@ -54,15 +48,11 @@ app.delete('/api/activities', function(req, res) {
     }
   });
 });
-
 var port = process.env.PORT || 3000;
 // var ip = process.env.IP || 'localhost';
-
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
-
-
 //=======================DYNAMICALLY CONNECT TO DEV VS LIVE MONGODB===========================
 //
 // var uristring =
