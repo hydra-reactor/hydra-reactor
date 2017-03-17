896790b6b6055130fe29b750478f071b95fcc25b
Ryan Freude <freudehack@gmail.com>
2017-03-17 14:04:49 -0700

    cleaned up server file

--- server/server.js
+++ server/server.js
@@ -7,31 +7,9 @@ var {authenticate} = require('./middleware/authenticate');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-<<<<<<< 69dd2b6292edeb37500b6ddf0a6c69e6358655bb
 // mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 mongoose.connect('mongodb://localhost/hydra');
 // var db = mongoose.connection;
-=======
-
-
-mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
-//mongoose.connect('mongodb://localhost/hydra');
-var db = mongoose.connection;
-
-Activity.create({
-  description: 'Ryan\'s Terrible Tacos',
-  category: 'food'
-});
-
-// Code example included in https://github.com/expressjs/body-parser
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// })
-
-// Serve static files. NEED TO UPDATE PATH
->>>>>>> updated mongodb and json
 app.use(express.static(path.join(__dirname, '../client/')));
 // Set up POST request listener for creating a new user
 // Expects to receive email and password in req.body
@@ -155,33 +133,3 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
-
-//mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98
-
-// var pg = require('pg');
-
-// app.get('/db', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//     client.query('SELECT * FROM test_table', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// });
-//
-//
-// var uristring =
-//     process.env.MONGOLAB_URI ||
-//     process.env.MONGOHQ_URL ||
-//     'mongodb://localhost/HelloMongoose';
-//
-//     mongoose.connect(uristring, function (err, res) {
-//       if (err) {
-//       console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//       } else {
-//       console.log ('Succeeded connected to: ' + uristring);
-//       }
-//     });
