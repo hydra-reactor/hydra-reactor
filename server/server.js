e71559e4bbee936b29c54346fcdaaab53d30ccc7
Emma Rios <tigersoundtracks@gmail.com>
2017-03-17 12:29:26 -0600

    Completed MVP front end work

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 //mongoose.connect('mongodb://localhost/hydra');
 var db = mongoose.connection;
+
 app.use(express.static(path.join(__dirname, '../client/')));
 //Set up GET request listener for retrieving links
 app.get('/api/activities', function(req, res) {
