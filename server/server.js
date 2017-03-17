f0e19463b57a97126b7ba499617d915631f1bcb6
Ryan Freude <freudehack@gmail.com>
2017-03-17 15:03:31 -0700

    Updated server and json file

--- server/server.js
+++ server/server.js
@@ -8,16 +8,6 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-<<<<<<< 3e12e97917b4b083d00883b0df656c89a3fd190e
-=======
-//mongoose.connect('mongodb://localhost/hydra');
-//var db = mongoose.connection;
-
-// Activity.create({
-//   description: 'Ryan\'s Terrible Tacos',
-//   category: 'food'
-// });
->>>>>>> temp commit
 
 mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 //mongoose.connect('mongodb://localhost/hydra');
@@ -26,7 +16,6 @@ var db = mongoose.connection;
 // Serve static files.
 app.use(express.static(path.join(__dirname, '../client/')));
 
-<<<<<<< 3e12e97917b4b083d00883b0df656c89a3fd190e
 //Set up GET request listener for retrieiving links
 app.get('/api/activities', function(req, res) {
   console.log('req.body: ', req.body);
@@ -35,25 +24,15 @@ app.get('/api/activities', function(req, res) {
     res.json(data);
   });
 });
-=======
-// Set up GET request listener for retrieiving links
-// app.get('/api/activities', function(req, res) {
-//   console.log('req.body: ', req.body);
-//   Activity.find(function(err, data) {
-//     console.log('data: ', data);
-//     res.json(data);
-//   });
-// });
->>>>>>> temp commit
 
-// // Set up POST request listener for adding a link
-// app.post('/api/activities', function(req, res) {
-//   console.log('req.body: ', req.body);
-//   Activity.create(req.body, function(err, data) {
-//     console.log('data: ', data);
-//     res.json(data);
-//   });
-// });
+// Set up POST request listener for adding a link
+app.post('/api/activities', function(req, res) {
+  console.log('req.body: ', req.body);
+  Activity.create(req.body, function(err, data) {
+    console.log('data: ', data);
+    res.json(data);
+  });
+});
 
 var port = process.env.PORT || 3000;
 // var ip = process.env.IP || 'localhost';
