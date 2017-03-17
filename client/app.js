a5134b5176d415b62059f4fe1ad2ca9995689767
Emma Rios <tigersoundtracks@gmail.com>
2017-03-17 12:29:26 -0600

    Completed MVP front end work

--- client/app.js
+++ client/app.js
@@ -1,30 +1,42 @@
 angular.module('hydraApp', []).controller('activityController', function($scope, $http) {
-  $scope.activity = '';
-  $scope.activityType = '';
+  $scope.description = '';
+  $scope.category = '';
   $scope.storage = [];
   // $scope.hello = 'Hail Hydra!';
 
+  // if there's no text in the input field, don't do the post request
+  // empty the input field after a post request
   $scope.postNewData = function() {
     var req = {
       method: 'POST',
-      url: '/',
+      url: '/api/activities',
       data: {
-        activity: $scope.activity,
-        activityType: $scope.activityType
-        }
-      };
+        description: $scope.description,
+        category: $scope.category
+      }
+    };
     $http(req);
-    // .then(function successCallback(response) {
-    //     console.log(response);
-    //     // this callback will be called asynchronously
-    //     // when the response is available
-    //   }, function errorCallback(error) {
-    //     console.log('error!');
-    //   });
-  }
+  };
+  $scope.getData = function() {
+    var req = {
+      method: 'GET',
+      url: '/api/activities'
+    };
+    $http(req)
+    .then(function successCallback(response) {
+      $scope.storage = response.data;
+      console.log(response);
+        // this callback will be called asynchronously
+        // when the response is available
+    }, function errorCallback(error) {
+      console.log('error!');
+    });
+  };
 });
 
+// every time getData is run, clear our the storage and then push our response into storage
 
+// use ng-repeat on the index.html page to display each item in storage
 
 
 
