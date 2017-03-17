7466e19c7ebb47a10c9ae7a1456e166c530c13c6
Darin Allen <darinallen@gmail.com>
2017-03-17 18:20:27 -0400

    Minor updates

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 //mongoose.connect('mongodb://localhost/hydra');
 var db = mongoose.connection;
