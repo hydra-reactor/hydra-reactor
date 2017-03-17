b8fdd52637b92956114ce068c81865c620b0d575
Ryan Freude <freudehack@gmail.com>
2017-03-17 11:06:07 -0700

    temp commit

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/hydra');
-var db = mongoose.connection;
+//mongoose.connect('mongodb://localhost/hydra');
+//var db = mongoose.connection;
 
 // Activity.create({
 //   description: 'Ryan\'s Terrible Tacos',
@@ -27,22 +27,22 @@ var db = mongoose.connection;
 app.use(express.static(path.join(__dirname, '../client/')));
 
 // Set up GET request listener for retrieiving links
-app.get('/api/activities', function(req, res) {
-  console.log('req.body: ', req.body);
-  Activity.find(function(err, data) {
-    console.log('data: ', data);
-    res.json(data);
-  });
-});
+// app.get('/api/activities', function(req, res) {
+//   console.log('req.body: ', req.body);
+//   Activity.find(function(err, data) {
+//     console.log('data: ', data);
+//     res.json(data);
+//   });
+// });
 
-// Set up POST request listener for adding a link
-app.post('/api/activities', function(req, res) {
-  console.log('req.body: ', req.body);
-  Activity.create(req.body, function(err, data) {
-    console.log('data: ', data);
-    res.json(data);
-  });
-});
+// // Set up POST request listener for adding a link
+// app.post('/api/activities', function(req, res) {
+//   console.log('req.body: ', req.body);
+//   Activity.create(req.body, function(err, data) {
+//     console.log('data: ', data);
+//     res.json(data);
+//   });
+// });
 
 var port = process.env.PORT || 3000;
 // var ip = process.env.IP || 'localhost';
