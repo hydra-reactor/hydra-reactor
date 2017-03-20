24062f7e14e11c1febe6626fb6983058451d3103
Darin Allen <darinallen@gmail.com>
2017-03-20 18:22:37 -0400

    Refactor $http requests in new factory, and add delete functionality

--- server/server.js
+++ server/server.js
@@ -17,19 +17,33 @@ app.use(express.static(path.join(__dirname, '../client/')));
 
 //Set up GET request listener for retrieiving links
 app.get('/api/activities', function(req, res) {
-  console.log('req.body: ', req.body);
+  console.log('Server GET req.body: ', req.body);
   Activity.find(function(err, data) {
-    console.log('data: ', data);
+    // console.log('Server GET data: ', data);
     res.json(data);
   });
 });
 
 // Set up POST request listener for adding a link
 app.post('/api/activities', function(req, res) {
-  console.log('req.body: ', req.body);
+  console.log('Server POST req.body: ', req.body);
   Activity.create(req.body, function(err, data) {
     console.log('data: ', data);
     res.json(data);
+  });
+});
+
+app.delete('/api/activities', function(req, res) {
+  console.log('Server DELETE req.body: ', req.body);
+  Activity.remove({
+    '_id': req.body._id
+  }, function( err, removed) {
+    if (err) {
+      console.log('Error!');
+    } else {
+      // console.log('Server DELETE response: ', removed);
+      res.send(removed);
+    }
   });
 });
 
