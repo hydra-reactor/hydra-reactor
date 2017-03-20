a09696ff61173e0e544128b059a6df95861085a1
Darin Allen <darinallen@gmail.com>
2017-03-20 18:22:37 -0400

    Refactor $http requests in new factory, and add delete functionality

--- client/app.js
+++ client/app.js
@@ -1,6 +1,66 @@
-angular.module('hydraApp', [])
+var app = angular.module('hydraApp', []);
 
-.controller('activityController', ['$scope', '$http', function($scope, $http) {
+app.factory('Activities', function($http) {
+
+  var postNewData = function(description, category, storage) {
+    var req = {
+      method: 'POST',
+      url: '/api/activities',
+      data: {
+        description: description,
+        category: category
+      }
+    };
+    $http(req)
+    // the following will be called asynchronously when the response is available
+    .then(function successCallback(response) {
+      console.log('postNewData success');
+      storage.push(response.data);
+    }, function errorCallback(error) {
+      console.log('error!');
+    });
+  };
+
+  var getData = function() {
+    var req = {
+      method: 'GET',
+      url: '/api/activities'
+    };
+    return $http(req)
+    // the following will be called asynchronously when the response is available
+    .then(function successCallback(response) {
+      console.log('Client getData response.data: ', response.data);
+      return response.data;
+    }, function errorCallback(error) {
+      console.log('error!');
+    });
+  };
+
+  var deleteActivity = function(activity) {
+    var req = {
+      method: 'DELETE',
+      url: '/api/activities',
+      data: activity
+    };
+    console.log('delete Activity request: ', req);
+    $http(req)
+    // the following will be called asynchronously when the response is available
+    .then(function successCallback(response) {
+      console.log('Client deleteActivity response: ', response);
+    }, function errorCallback(error) {
+      console.log('error!');
+    });
+  };
+
+  return {
+    postNewData: postNewData,
+    getData: getData,
+    deleteActivity: deleteActivity
+  };
+
+});
+
+app.controller('activityController', ['$scope', '$http', 'Activities', function($scope, $http, Activities) {
   $scope.description = '';
   $scope.category = '';
   $scope.storage = [];
@@ -8,42 +68,17 @@ angular.module('hydraApp', [])
 
   // if there's no text in the input field, don't do the post request
   // empty the input field after a post request
-  $scope.postNewData = function() {
-    var req = {
-      method: 'POST',
-      url: '/api/activities',
-      data: {
-        description: $scope.description,
-        category: $scope.category
-      }
-    };
-    $http(req)
-    .then(function successCallback(response) {
-      $scope.storage.push(response.data);
-      // console.log($scope.storage);
-        // this callback will be called asynchronously
-        // when the response is available
-    }, function errorCallback(error) {
-      console.log('error!');
-    });
-  };
+  $scope.postNewData = Activities.postNewData;
+  $scope.getData = Activities.getData;
+  $scope.deleteActivity = Activities.deleteActivity;
 
-  $scope.getData = function() {
-    var req = {
-      method: 'GET',
-      url: '/api/activities'
-    };
-    $http(req)
-    .then(function successCallback(response) {
-      $scope.storage = response.data;
-      // console.log(response);
-        // this callback will be called asynchronously
-        // when the response is available
-    }, function errorCallback(error) {
-      console.log('error!');
-    });
-  };
-  $scope.getData();
+  $scope.getData().then(function(data) {
+    $scope.storage = data;
+  });
+
+  // This code is needed to send data in an $http DELETE request.
+  // Stack overflow issue that addresses this: http://stackoverflow.com/questions/37796227/body-is-empty-when-parsing-delete-request-with-express-and-body-parser
+  $http.defaults.headers.delete = { "Content-Type": "application/json;charset=utf-8" };
 }]);
 
 // every time getData is run, clear our the storage and then push our response into storage
