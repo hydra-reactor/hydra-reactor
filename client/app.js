fb94b991318b63d6b68fd3585a1af5c77fde1d2a
Emma Rios <tigersoundtracks@gmail.com>
2017-03-23 16:46:11 -0600

    Updated login-controller.js to reflect correct module and controller names.

--- client/app.js
+++ client/app.js
@@ -1,42 +1,37 @@
 var app = angular.module('hydraApp', []);
-
 app.factory('User', function($http) {
-
 //storage object, which will be equal to 'data' from an initial get request once the user signs in
   var userData = {};
   //get request to pull in user info
   var newSignUp = function(email, password) {
+    console.log('newSignUp is getting invoked!');
     var req = {
       method: 'POST',
       url: '/api/signup',
       data: {
-        email: email,
-        password: password
+        "email": email,
+        "password": password
       }
     };
     $http(req)
       // the following will be called asynchronously when the response is available
       .then(function successCallback(response) {
         console.log('newSignUp success');
+        console.dir(response);
+        console.log('the returned data is:' + response.data);
         userData = response.data;
+        console.log(userData);
       }, function errorCallback(error) {
         console.log('error!');
       });
   };
-
   return {
     userData: userData,
     newSignUp: newSignUp
 //     deleteActivity: deleteActivity
   };
-
+  var newTrip = function() {};
 });
-
-app.controller('activityController', ['$scope', '$http', 'User', function($scope, $http, User) {
-  $scope.email = '';
-  $scope.password = '';
-  $scope.newSignUp = User.newSignUp;
-}]);
 
 
 // app.factory('Activities', function($http) {
