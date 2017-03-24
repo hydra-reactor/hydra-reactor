8f968d95e4f58025170d14dd7a97c9e03b9102d2
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-03-24 23:15:56 +1100

    Add client authentication

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,25 +1,142 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-    // unique: true
-  },
-  password: {
-    type: String
-  },
-  trips: [{
-    tripName: String,
-    days: [{
-      dayName: String,
-      activities: [{
-        activityName: String,
-        category: String
-      }]
-    }]
-  }]
+mongoose.Promise = global.Promise;
+
+var Schema = mongoose.Schema;
+
+var ActivitySchema = new Schema({
+  description: String,
+  category: String
 });
 
-module.exports = mongoose.model('User', UserSchema);
+var DaySchema = new Schema({
+  day: Number,
+  activities: [ActivitySchema]
+});
+
+var TripSchema = new Schema({
+  tripName: String,
+  days: [DaySchema]
+});
+
+var UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    // unique: true,
+    validate: {
+      isAsync: true,
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true
+    // minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String
+      // required: true
+    },
+    token: {
+      type: String
+      // required: true
+    }
+  }],
+  trips: [TripSchema]
+});
+
+
+// methods - instance methods
+UserSchema.methods.generateToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toString(), access}, 'somesecret');
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+
+// statics - Model methods
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'somesecret');
+  } catch (err) {
+    return Promise.reject(err);
+  }
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  // console.log('----- findByCredentials -----');
+  // console.log('User', User);
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      console.log('Promise.reject');
+      return Promise.reject();
+    }
+    // because bcrypt didn't support Promises, use generic Promise
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          console.log('Resolve bcrypt');
+          resolve(user);
+        } else {
+          console.log('Reject bcrypt');
+          reject();
+        }
+      });
+    });
+  });
+};
+
+// Mongoose middleware, runs before each 'save'
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
-var User = require('./user-model.js');
+
+var User = require('./models/userModel.js');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
 // mongoose.connect('mongodb://heroku_0fn1fg98:vi2sk4eagfo3dj3pbg1407vr0l@ds133450.mlab.com:33450/heroku_0fn1fg98/hydra');
 mongoose.connect('mongodb://localhost/hydra');
-var db = mongoose.connection;
+// var db = mongoose.connection;
+
 app.use(express.static(path.join(__dirname, '../client/')));
 
 // var testSignup = {
@@ -42,37 +47,71 @@ var testSignin = {
 //     "day_id": "", // Need to update with database data
 //   "activity_id": "" // Need to update with database data
 // };
+
 // Set up POST request listener for creating a new user
 // Expects to receive email and password in req.body
 app.post('/api/signup', function(req, res) {
   console.log('Received the following POST request to create a user: ', req.body);
   // Mongoose method to create a user
-  User.create(req.body, function(err, user) {
-    if(err) {
-      console.log('Error: ', err);
-    } else {
-      res.json(user);
-    }
+  var user = new User(req.body);
+
+  user.save().then(() => {
+    return user.generateToken();
+  }).then(token => {
+    res.header('x-auth', token).send(user);
+  }).catch(err => {
+    res.status(400).send(err);
   });
+
+    // User.create(req.body, function(err, user) {
+  //   if(err) {
+  //     console.log('Error: ', err);
+  //   } else {
+  //     res.json(user);
+  //   }
+  // });
+
+
 });
+
 // Set up POST request listener for signing in a user
 // Expects to receive a user_id in req.body
 app.post('/api/signin', function(req, res) {
+  var {email, password} = req.body;
+
   console.log('Received the following GET request for a user: ', req.body);
+
+  User.findByCredentials(email, password).then(user => {
+    return user.generateToken().then(token => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch(err => {
+    res.status(400).send(err);
+  });
+
   // Mongoose method to retrieve a user
-  User.findOne({
-    'email': req.body.email
-  }, function(err, user) {
-    if(err) {
-      console.log('Error: ', err)
-    } else {
-      res.json(user);
-    }
+  // User.findOne({
+  //   'email': req.body.email
+  // }, function(err, user) {
+  //   if(err) {
+  //     console.log('Error: ', err)
+  //   } else {
+  //     res.json(user);
+  //   }
+  // });
+});
+
+app.delete('/api/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
   });
 });
+
 // Set up POST request listener for creating a new trip
 // Expects to receive user_id and trip in req.body, where trip is an object with tripName and numDays properties
-app.post('/api/trips', function(req, res) {
+app.post('/api/trips', authenticate, function(req, res) {
   console.log('Received the following POST request to create a trip: ', req.body);
   // Mongoose method to retrieve and update a user
   User.findOneAndUpdate({'_id': req.body.user_id}, {$push: { trips: { tripName: req.body.trip.tripName } } }, {new: true}, function(err, user) {
@@ -100,16 +139,12 @@ app.post('/api/trips', function(req, res) {
 // Set up POST request listener for creating a new activity
 // Expects to receive user_id, trip_id, days_id, and activity in req.body,
 // where activity is an object with description and category properties
-app.post('/api/activities', function(req, res) {
+app.post('/api/activities', authenticate, function(req, res) {
   // Pass in request object that includes user id, trip object id, day object id, activity object
   console.log('Received the following POST request to create an activity: ', req.body);
   User.findById(req.body.user_id, function(err, user) {
     if(err) {
       console.log('Error: ', error);
-<<<<<<< 6dfe73150206e5eb87b0ddbc3ff142f57bfa9c96
-    } else {
-      user.trips.id(req.body.trip_id).days.id(req.body.day_id).activities.push(req.body.activity);
-=======
     } else {
       user.trips.id(req.body.trip_id).days.id(req.body.day_id).activities.push(req.body.activity);
       user.save();
@@ -117,28 +152,10 @@ app.post('/api/activities', function(req, res) {
     }
   });
 });
+
 // Set up DELETE request listener for deleting an activity
 // Expects to receive user_id, trip_id, days_id, and activity_id in req.body
-app.delete('/api/activities', function(req, res) {
-  console.log('Received the following DELETE request to delete an activity', req.body);
-  // Call Mongoose remove method on id matching the request
-  User.findById(req.body.user_id, function(err, user) {
-    if(err) {
-      console.log('Error: ', error);
-    } else {
-      // The following code splices an individual activity out of the activities array
-      var activities = user.trips.id(req.body.trip_id).days.id(req.body.day_id).activities;
-      activities.splice(activities.indexOf(activities.id(req.body.activity_id)), 1);
->>>>>>> Sync server.js
-      user.save();
-      res.json(user);
-    }
-  });
-});
-<<<<<<< 6dfe73150206e5eb87b0ddbc3ff142f57bfa9c96
-// Set up DELETE request listener for deleting an activity
-// Expects to receive user_id, trip_id, days_id, and activity_id in req.body
-app.delete('/api/activities', function(req, res) {
+app.delete('/api/activities', authenticate, function(req, res) {
   console.log('Received the following DELETE request to delete an activity', req.body);
   // Call Mongoose remove method on id matching the request
   User.findById(req.body.user_id, function(err, user) {
@@ -154,8 +171,6 @@ app.delete('/api/activities', function(req, res) {
   });
 });
 
-=======
->>>>>>> Sync server.js
 var port = process.env.PORT || 3000;
 // var ip = process.env.IP || 'localhost';
 app.listen(port, function() {
