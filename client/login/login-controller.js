81d53bd0ce312a6394ae42c1f2df3193b247e4ed
Ryan Freude <freudehack@gmail.com>
2017-03-23 17:30:04 -0700

    test

--- client/login/login-controller.js
+++ client/login/login-controller.js
@@ -1,6 +1,10 @@
 
 
+<<<<<<< 93b6945f8dfbf8f30f3b83bb70faaf94ae0b25fa
 angular.module('loginModule', ['hydraApp']).controller('loginController', ['$scope', '$http', 'User', function($scope, $http, User) {
+=======
+angular.module('loginSplash', []).controller('activityController', ['$scope', '$http', 'User', function($scope, $http, User) {
+>>>>>>> misc commit
   $scope.email = '';
   // $scope.password = '';
   $scope.signIn = User.signIn;
